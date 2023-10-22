d7c95642b599b06b294677017c42c63453265363
jose32005 <122274991+jose32005@users.noreply.github.com>
2023-10-22 11:41:25 -0400

    Add files via upload

--- Proyecto1-EDD/src/main.java
+++ Proyecto1-EDD/src/main.java
@@ -1,6 +1,6 @@
 import EDD.Grafo;
 import Extras.Funciones;
-import Interfaces.Ventana1;
+
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
@@ -18,28 +18,20 @@ public class main {
      */
     public static void main(String[] args) {
         
-        Ventana1 ventana = new Ventana1();
-        ventana.setLocationRelativeTo(null);
-        ventana.setVisible(true);
-        
         Funciones func = new Funciones();
-        Grafo miGrafo = func.leer_txt();
-        //miGrafo.agregarNodo("@jose");
-        //miGrafo.conectarNodos("@jose", "@pepe");
-        //miGrafo.conectarNodos("@pepe", "@jose");
-        //miGrafo.conectarNodos("@jose", "@mazinger");
+        //func.abrir_archivo();
+        Grafo miGrafo = func.llenar_grafo();
+        
+        /*miGrafo.agregarNodo("@jose");
+        miGrafo.conectarNodos("@jose", "@pepe");
+        miGrafo.conectarNodos("@pepe", "@jose");
+        miGrafo.conectarNodos("@jose", "@mazinger");*/
         //miGrafo.eliminarNodo("@jose");
-        //func.escribir_txt(miGrafo);
-        
-        
-        
-        
-        
+        //func.guardar_archivo(miGrafo);
 
-        
         //miGrafo.imprimirGrafo();
         //miGrafo.Traspuesto().imprimirGrafo();
-        miGrafo.obtenerSCCs();
+        //miGrafo.obtenerSCCs();
         
   
     }
