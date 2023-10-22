e3b9a8d3664d7907b0265324d1aeef76d07537e2
fefi05 <129181491+fefi05@users.noreply.github.com>
2023-10-21 21:24:36 -0400

    Add files via upload

--- Proyecto1-EDD/src/main.java
+++ Proyecto1-EDD/src/main.java
@@ -1,6 +1,6 @@
 import EDD.Grafo;
 import Extras.Funciones;
-
+import Interfaces.Ventana1;
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
@@ -17,6 +17,10 @@ public class main {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        
+        Ventana1 ventana = new Ventana1();
+        ventana.setLocationRelativeTo(null);
+        ventana.setVisible(true);
         
         Funciones func = new Funciones();
         Grafo miGrafo = func.leer_txt();
